a767fe40fcf91dd878c4eab1fd84c2e166cf01b3
Janaledro <alejandro.garfide@ciencias.unam.mx>
2018-06-05 22:17:08 -0500

    Modifications to the metadata table reordering module and an update to the download URL

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 BASE_URL="https://www.ncbi.nlm.nih.gov/pubmed/?term="
 #BASE_URL2="sci-hub.tw/"
 #BASE_URL2="sci-hub.hk/"
-BASE_URL2="sci-hub.ru/"
+BASE_URL2="sci-hub.nu/"
 
 ## PATH to scihub.py script
 SCIHUB_PY="/home/iaguilar/bin/scihub.py/scihub/scihub.py"
